205d6d24b182545633a07c7d93beabd7fb6cbc8f
MertAltas <mertaltas@ymail.com>
2018-12-01 21:53:32 +0300

    first commit

---
+++ SpringTry/src/main/java/com/springreact/spirngtry/SpirngtryApplication.java
@@ -0,0 +1,12 @@
+package com.springreact.spirngtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngtryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngtryApplication.class, args);
+    }
+}
