affd13d9c89e937dea123d2ba949a1bb254de6fc
MertAltas <mertaltas@ymail.com>
2018-12-02 20:31:30 +0300

    project repo

--- SpringTry/src/main/java/com/springreact/spirngtry/Domain/Project.java
+++ SpringTry/src/main/java/com/springreact/spirngtry/Domain/Project.java
@@ -1,5 +1,98 @@
 package com.springreact.spirngtry.Domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Project {
-     public void  test(){}
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String prjectName;
+    private String projectIdentifer;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_at;
+    private Date updated_at;
+
+
+    public Project(){}
+
+    @PrePersist
+    protected  void onCreate(){
+        this.created_at = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrjectName() {
+        return prjectName;
+    }
+
+    public void setPrjectName(String prjectName) {
+        this.prjectName = prjectName;
+    }
+
+    public String getProjectIdentifer() {
+        return projectIdentifer;
+    }
+
+    public void setProjectIdentifer(String projectIdentifer) {
+        this.projectIdentifer = projectIdentifer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_at = new Date();
+    }
+
 }
