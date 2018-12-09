5d947aa2738b4abc2b9d2949e40e255f80ea67c2
MertAltas <mertaltas@ymail.com>
2018-12-09 18:32:59 +0300

    validation

--- SpringTry/src/main/java/com/springreact/spirngtry/Domain/Project.java
+++ SpringTry/src/main/java/com/springreact/spirngtry/Domain/Project.java
@@ -1,6 +1,11 @@
 package com.springreact.spirngtry.Domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,13 +14,22 @@ public class Project {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String prjectName;
+    @NotBlank(message = "project Identifier is required")
+    @Size( min=4,max=5,message = "please use 4 to 5 charcters")
+    @Column(updatable = false,unique = true)
     private String projectIdentifer;
+    @NotBlank(message = "Description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_at;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_at;
 
 
